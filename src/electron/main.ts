e84b0c9045b62fd9f6daca3a160094f85c5bb71d
garibcatastrofe <ramsesgaribflorescuen@gmail.com>
2026-04-27 21:55:39 -0700

    first commit

---
+++ src/electron/main.ts
@@ -0,0 +1,73 @@
+import { app, BrowserWindow, Menu } from "electron";
+import { ipcMainHandle, ipcMainOn, isDev } from "./util.js";
+import { getStaticData, pollResources } from "./resourceManager.js";
+import { getPreloadPath, getUIPath } from "./pathResolver.js";
+import { createTray } from "./tray.js";
+import { createMenu } from "./menu.js";
+
+Menu.setApplicationMenu(null);
+
+app.on("ready", () => {
+  const mainWindow = new BrowserWindow({
+    webPreferences: {
+      preload: getPreloadPath(),
+    },
+    // No quitar si queremos el menu
+    frame: true,
+  });
+
+  if (isDev()) {
+    mainWindow.loadURL("http://localhost:3002");
+  } else {
+    mainWindow.loadFile(getUIPath());
+  }
+
+  pollResources(mainWindow);
+
+  ipcMainHandle("getStaticData", () => {
+    return getStaticData();
+  });
+
+  ipcMainOn("sendFrameAction", (payload) => {
+    switch (payload) {
+      case "CLOSE":
+        mainWindow.close();
+        break;
+      case "MAXIMIZE":
+        mainWindow.maximize();
+        break;
+      case "MINIMIZE":
+        mainWindow.minimize();
+        break;
+    }
+  });
+
+  createTray(mainWindow);
+  handleCloseEvents(mainWindow);
+  if (isDev()) {
+    createMenu(mainWindow);
+  }
+});
+
+function handleCloseEvents(mainWindow: BrowserWindow) {
+  let willClose = false;
+
+  mainWindow.on("close", (e) => {
+    if (willClose) {
+      return;
+    }
+    e.preventDefault();
+    mainWindow.hide();
+    if (app.dock) {
+      app.dock.hide();
+    }
+  });
+
+  app.on("before-quit", () => {
+    willClose = true;
+  });
+
+  mainWindow.on("show", () => {
+    willClose = false;
+  });
+}
---
+++ src/electron/util.ts
@@ -0,0 +1,53 @@
+import { ipcMain, WebContents, WebFrameMain } from "electron";
+import { getUIPath } from "./pathResolver.js";
+import { pathToFileURL } from "url";
+
+export function isDev(): boolean {
+  return process.env.NODE_ENV === "development";
+}
+
+export function ipcMainHandle<Key extends keyof EventPayloadMapping>(
+  key: Key,
+  handler: () => EventPayloadMapping[Key],
+) {
+  ipcMain.handle(key, (event) => {
+    if (!event.senderFrame) {
+      throw new Error("Frame inválido o no disponible");
+    }
+
+    validateEventFrame(event.senderFrame);
+    return handler();
+  });
+}
+
+export function ipcMainOn<Key extends keyof EventPayloadMapping>(
+  key: Key,
+  handler: (payload: EventPayloadMapping[Key]) => void,
+) {
+  ipcMain.on(key, (event, payload) => {
+    if (!event.senderFrame) {
+      throw new Error("Frame inválido o no disponible");
+    }
+
+    validateEventFrame(event.senderFrame);
+    return handler(payload);
+  });
+}
+
+export function ipcWebContentsSend<Key extends keyof EventPayloadMapping>(
+  key: Key,
+  webContents: WebContents,
+  payload: EventPayloadMapping[Key],
+) {
+  webContents.send(key, payload);
+}
+
+export function validateEventFrame(frame: WebFrameMain) {
+  if (isDev() && new URL(frame.url).host === "localhost:3002") {
+    return;
+  }
+
+  if (frame.url !== pathToFileURL(getUIPath()).toString()) {
+    throw new Error("Evento malicioso");
+  }
+}
